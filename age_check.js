3683cf036fb6ce1fd2cc6a8d569bb8a7a013ad3d
k0dm1 <47019105+k0dm1@users.noreply.github.com>
2019-01-24 23:13:49 -0500

    Update age_check.js

--- age_check.js
+++ age_check.js
@@ -91,3 +91,24 @@ page_html(){
 }
 
 }
+
+
+
+
+
+
+// Javascript Function to pass Jquery function call to class
+function call_back_kodmi(){
+	 $.force_appear(); // Jquery function call
+}
+// End of Javascript Function to pass Jquery function call to class
+
+var callback = call_back_kodmi; // sets your callback variable as your javascript function.
+
+const age_pop = new age_verify(
+callback, // Callback Function | This excutes if Age is successfully validated, or session is already set.
+18, // Minimum Required Age
+null, // Custom  Html for age checker.
+null, // Custom Css for age checker.
+null //  Custom Html for sorry message.
+);
