90b0d795029c3e1e1a7e17cf647aa92b8c0aa52e
k0dm1 <47019105+k0dm1@users.noreply.github.com>
2019-01-26 14:34:50 -0500

    Add files via upload
    Wordpress Plugin Master Release V1.0

--- KodMi-Age-Validator-WordPress/kodmi-age-validator.php
+++ KodMi-Age-Validator-WordPress/kodmi-age-validator.php
@@ -10,16 +10,17 @@
  *
  * @link              https://kodmi.com/
  * @since             1.0.0
- * @package           KodMi_Age_Validator
+ * @package           Kodmi_Age_Validator
  *
  * @wordpress-plugin
  * Plugin Name:       KodMi Age Validator
- * Description:       A responsive, no dependency, age checking plugin that doesn't conflict with your current theme. Check the age of every single visitor on your wordpress site with this amazing KodMi Plugin.
+ * Plugin URI:        https://kodmi.com/
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            KodMi
  * Author URI:        https://kodmi.com/
  * License:           GPL-2.0+
- * License URI:       http://www.gnu.orge/licenses/gpl-2.0.txt
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       kodmi-age-validator
  * Domain Path:       /languages
  */
@@ -34,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'KodMi Age Validator', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
